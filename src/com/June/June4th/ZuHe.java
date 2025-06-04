975fe627653a7a581c74d5231ff6ecf3c2df35d8
熊康 <12158434+qingli--house@user.noreply.gitee.com>
2025-06-04 22:43:21 +0800

    6月4日提交

--- src/com/June/June4th/ZuHe.java
+++ src/com/June/June4th/ZuHe.java
@@ -1,5 +1,28 @@
 package com.June.June4th;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //回溯章
 //组合
 public class ZuHe {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        backtracking(n,k,path,result,1);
+        return result;
+    }
+    public void backtracking(int n,int k,List<Integer> path,List<List<Integer>> result,int startIndex){
+        if(path.size() == k){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = startIndex; i <= n; i ++){
+            path.add(i);
+            //递归
+            backtracking(n,k,path,result,i + 1);
+            //回溯
+            path.remove(path.size() - 1);
+        }
+    }
 }
