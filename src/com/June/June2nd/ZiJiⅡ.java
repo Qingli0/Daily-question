9cd2f14d7a3d301cb07574c9beaa34cbe3730a93
熊康 <12158434+qingli--house@user.noreply.gitee.com>
2025-06-10 14:41:34 +0800

    6月2日提交 - 子集Ⅱ

---
+++ src/com/June/June2nd/ZiJiⅡ.java
@@ -0,0 +1,35 @@
+package com.June.June2nd;
+//回溯篇
+//子集Ⅱ
+import java.util.*;
+
+
+public class ZiJiⅡ {
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Arrays.sort(nums);
+        backtracking(result,path,0,nums);
+        return new ArrayList<>(result);
+
+}
+    public static void backtracking(List<List<Integer>> result,List<Integer> path,int startIndex,int[] nums){
+        result.add(new ArrayList<>(path));
+        if(startIndex >= nums.length){
+            return;
+        }
+        for(int i = startIndex; i < nums.length; i ++){
+            if (i > startIndex && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            path.add(nums[i]);
+            backtracking(result,path,i + 1,nums);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,4,4,1,4};
+        System.out.println(subsetsWithDup(nums));
+    }
+}
