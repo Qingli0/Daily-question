0c1f5e5849418d5b64be70499a33028dd8a6da64
kangzi <12158434+qingli--house@user.noreply.gitee.com>
2025-07-12 17:57:08 +0800

    7月13日提交 - 零钱兑换Ⅱ

--- src/com/July/July13th/LingQianDuiHuanⅡ.java
+++ src/com/July/July13th/LingQianDuiHuanⅡ.java
@@ -21,10 +21,22 @@ public class LingQianDuiHuanⅡ {
 
         return dp[coins.length - 1][amount];
     }
-
+    //滚动数组方法求解
+    public static int change2(int amount, int[] coins) {
+        //采用一维数组 递推公式dp[j] += dp[j - coins[i]]
+        int[] dp = new int[amount + 1];
+        dp[0]  = 1;
+        for(int i = 0; i < coins.length; i ++){
+            for(int j = coins[i]; j <= amount; j ++){
+                    dp[j] += dp[j - coins[i]];
+            }
+        }
+        return dp[amount];
+    }
     public static void main(String[] args) {
         int amount = 4681;
         int coins[] = {10,1};
+        System.out.println(change2(amount, coins));
         System.out.println(change(amount, coins));
     }
 }
