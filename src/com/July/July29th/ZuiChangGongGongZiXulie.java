a5d26d1e59db671d344d10dc01ece1adf73b87c2
kangzi <12158434+qingli--house@user.noreply.gitee.com>
2025-07-25 09:12:33 +0800

    7月29日提交 - 最长重复子序列

---
+++ src/com/July/July29th/ZuiChangGongGongZiXulie.java
@@ -0,0 +1,30 @@
+package com.July.July29th;
+//动态规划
+//最长公共子序列
+
+/**
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。
+ * 如果不存在 公共子序列 ，返回 0 。
+ *
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ */
+public class ZuiChangGongGongZiXulie {
+    public static int longestCommonSubsequence(String text1, String text2) {
+        //也是dp[i][j]
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        int res = 0;
+        for(int i = 1; i <= text1.length(); i ++){
+            for(int j = 1; j <= text2.length(); j ++){
+                if(text1.charAt(i - 1) == text2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i - 1][j],dp[i][j - 1]);
+                res = Math.max(res,dp[i][j]);
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        String text1 = "abcde";
+        String text2 = "abde";
+        System.out.println(longestCommonSubsequence(text1, text2));
+    }
+}
